3f7625f4566b47e5a38147c1eba7f515382f9418
kipkemoilvs <kipkemoilvs@gmail.com>
2023-02-01 11:27:54 +0300

    fx::listings bug fix

---
+++ app/Models/Listing.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Models;
+
+class Listing {
+    public static function all()
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Listing one',
+                'description' => 'Listing oneListing oneListing oneListing oneListing oneListing oneListing one'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing two',
+                'description' => 'Listing oneListing oneListing oneListing oneListing oneListing oneListing one'
+            ]
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,73 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+use App\Models\Listing;
 
 Route::get('/', function () {
     return view('listings', [
         'heading' => 'Latest Listings',
-        'listings' => [
-            [
-                'id' => 1,
-                'title' => 'Listing one',
-                'description' => 'Listing oneListing oneListing oneListing oneListing oneListing oneListing one'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing two',
-                'description' => 'Listing oneListing oneListing oneListing oneListing oneListing oneListing one'
-            ]
-        ]
+        'listings' => Listing::all()
     ]);
 });
-
-
-// Route::get('/', function () {
-//     return view('listings',
-
-//     [
-//         'heading' => 'Latest Listings',
-//         'listings' => 
-//         [
-//             'id' => 1,
-//             'title' => 'Listing one',
-//             'description' => 'Listing oneListing oneListing oneListing oneListing oneListing oneListing one
-//         '
-        
-//         ],
-
-//         [
-//             'id' => 2,
-//             'title' => 'Listing two',
-//             'description' => 'Listing oneListing oneListing oneListing oneListing oneListing oneListing one
-//         '
-//         ]
-//     ]);
-
-// });
-
-// Route::get('hello', function () {
-//     return response('hello world');
-// });
-
-
-// Route::get('posts/{id}', function ($id) {
-//     // ddd($id);
-//     return response(('Post' . $id));
-// })->where('id', '[0-9]+');
-
-
-// Route::get('/search', function(Request $request) {
-//     return($request->name . '' . $request->city);        
-// });
